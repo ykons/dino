456b7900e2aed781f9b4dbbc9c0ca123124ddbb4
Alexandro Silva <alexandro@Alexandros-iMac.local>
2020-09-22 23:37:19 -0300

    Replacing numbers with defined constants

--- layer/HorizonLayer.py
+++ layer/HorizonLayer.py
@@ -3,17 +3,17 @@ import random
 from .Layer import Layer
 
 from sprites import Cloud
+import utils.values as vl
 
 
 class HorizonLayer(Layer):
     def __init__(self, gameState):
         self.gameState = gameState
         self.clouds = self.gameState.clouds
-        self.clouds.add(Cloud(1100, 10))
 
     def update(self):
         if len(self.clouds) < 5 and random.randrange(0, 300) == 10:
-            self.clouds.add(Cloud(1100, random.randrange(300/5, 300/2)))
+            self.clouds.add(Cloud(vl.WORLD_WIDTH, random.randrange(vl.WORLD_HEIGHT/5, vl.WORLD_HEIGHT/2)))
 
         # Update Sprites
         self.gameState.clouds.update()
