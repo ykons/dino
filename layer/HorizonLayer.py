f0324e70943e18cebd0bdedbb94e8454c7c62f79
Alexandro Silva <alexandro@Alexandros-iMac.local>
2020-09-23 21:54:19 -0300

    Cloud randomization algorithm has been updated

--- layer/HorizonLayer.py
+++ layer/HorizonLayer.py
@@ -7,16 +7,40 @@ import utils.values as vl
 
 
 class HorizonLayer(Layer):
+    BG_CLOUD_SPEED = 0.2
+    BUMPY_THRESHOLD = .3
+    CLOUD_FREQUENCY = .5
+    HORIZON_HEIGHT = 16
+    MAX_CLOUDS = 6
+
     def __init__(self, gameState):
         self.gameState = gameState
         self.clouds = self.gameState.clouds
+        self.cloudFrequency = self.CLOUD_FREQUENCY
+        self.cloudSpeed = self.BG_CLOUD_SPEED
+
+    def addCloud(self):
+        self.clouds.add(Cloud(vl.WORLD_WIDTH, random.randrange(Cloud.MAX_SKY_LEVEL, Cloud.MIN_SKY_LEVEL)))
+
+    def updateClouds(self, deltaTime, speed):
+        cloudSpeed = self.cloudSpeed / 1000 * deltaTime * speed
+        numClouds = len(self.clouds)
+
+        if numClouds:
+            self.clouds.update(cloudSpeed)
+
+            lastCloud = self.clouds.sprites()[-1]
+
+            # Check for adding a new cloud.
+            if (numClouds < self.MAX_CLOUDS and \
+                (vl.WORLD_WIDTH - lastCloud.rect.left) > lastCloud.cloudGap and \
+                self.cloudFrequency > random.random()):
+                self.addCloud()
+        else:
+            self.addCloud()
 
     def update(self):
-        if len(self.clouds) < 5 and random.randrange(0, 300) == 10:
-            self.clouds.add(Cloud(vl.WORLD_WIDTH, random.randrange(vl.WORLD_HEIGHT/5, vl.WORLD_HEIGHT/2)))
-
-        # Update Sprites
-        self.gameState.clouds.update()
+        self.updateClouds(.1, 3)
 
     def render(self, surface):
         self.gameState.clouds.draw(surface)
--- sprites/Cloud.py
+++ sprites/Cloud.py
@@ -1,17 +1,27 @@
+import random, math
+
 import pygame
 from utils import load_image
 
 
 class Cloud(pygame.sprite.Sprite):
+    SPRITE_X = 166
+    SPRITE_Y = 2
+    WIDTH = 92
+    HEIGHT = 27
+    MAX_CLOUD_GAP = 800
+    MAX_SKY_LEVEL = 60
+    MIN_CLOUD_GAP = 200
+    MIN_SKY_LEVEL = 142
+
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('dino_sprites.png', 166, 2, 92, 28, -1, -1, -1)
-        self.speed = 3
+        self.image, self.rect = load_image('dino_sprites.png', self.SPRITE_X, self.SPRITE_Y, self.WIDTH, self.HEIGHT, -1, -1, -1)
         self.rect.left = x
         self.rect.top = y
-        self.movement = [-1*self.speed, 0]
+        self.cloudGap = random.randint(self.MIN_CLOUD_GAP, self.MAX_CLOUD_GAP)
 
-    def update(self):
-        self.rect = self.rect.move(self.movement)
+    def update(self, speed):
+        self.rect = self.rect.move(-1*math.ceil(speed), 0)
         if self.rect.right < 0:
             self.kill()
